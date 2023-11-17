d3c515c49eb9027ec1cc352d55f474fc48ed6b6d
Ali Eren <91764741+alisokovic@users.noreply.github.com>
2023-11-17 21:54:54 +0000

    Last form of the project.

--- Test.cpp
+++ Test.cpp
@@ -8,14 +8,6 @@ int main()
 {
     List list1{5248522585};
 
-    /*Node* p1{list1.zeroth()};
-    for (int index{1} ; index <= 10 ; ++index)
-    {
-        list1.insert(index,p1);
-        p1 = p1->next;
-    }*/
-
-
     list1.print();
 
 
